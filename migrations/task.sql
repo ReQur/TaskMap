6395c5181eb4c2e61e81521cfd7c1473b5139333
Danil <dkhabibullin@cloudlinux.com>
2022-02-13 22:19:23 +0500

    INIT
    Initial commit. Add migrations for first tables. Add pakages file

---
+++ migrations/task.sql
@@ -0,0 +1,16 @@
+CREATE TABLE `task` (
+  `taskId` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `userId` VARCHAR(255),
+  `createdDate` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `label` VARCHAR(255),
+  `text` VARCHAR(512),
+  `color` VARCHAR(20),
+  `state` VARCHAR(20),
+  `coordinates` VARCHAR(255),
+  PRIMARY KEY (`userId`),
+  CONSTRAINT `fk_task_user_userId`
+    FOREIGN KEY (`userId`)
+    REFERENCES `user` (`userId`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION
+  );
