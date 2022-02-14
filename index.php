bb28f3877d0e39e07bffe7e29306556654ab91dc
Danil <dkhabibullin@cloudlinux.com>
2022-02-14 11:43:49 +0500

    MOVE connection to db in external of index file

--- index.php
+++ index.php
@@ -1,19 +1,5 @@
 <?php
-    require __DIR__ . '/vendor/autoload.php';
-    use Dotenv\Dotenv;
-
-    $dotenv = Dotenv::createImmutable(__DIR__);
-    $dotenv->safeLoad();
-    
-
-    try {
-      $conn = new PDO("mysql:host=$_ENV[servername];dbname=$_ENV[database]", $_ENV['username'], $_ENV['password']);
-      // set the PDO error mode to exception
-      $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-      echo "Connected successfully";
-    } catch(PDOException $e) {
-      echo "Connection failed: " . $e->getMessage();
-    }
+    require "dbconnect.php";
     echo '<br>';
     $result = $conn->query("SELECT * FROM user");
     while ($row = $result->fetch())
