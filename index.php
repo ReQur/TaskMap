fc1aa70ca7c1a5ad6c8f286dddc56d3d2faaef9e
Danil <dkhabibullin@cloudlinux.com>
2022-02-14 11:40:07 +0500

    ADD .env file

--- index.php
+++ index.php
@@ -1,11 +1,13 @@
 <?php
-    $servername = "localhost";
-    $username = "root";
-    $password = "rootPassword";
-    $database = "taskmap";
+    require __DIR__ . '/vendor/autoload.php';
+    use Dotenv\Dotenv;
+
+    $dotenv = Dotenv::createImmutable(__DIR__);
+    $dotenv->safeLoad();
+    
 
     try {
-      $conn = new PDO("mysql:host=$servername;dbname=$database", $username, $password);
+      $conn = new PDO("mysql:host=$_ENV[servername];dbname=$_ENV[database]", $_ENV['username'], $_ENV['password']);
       // set the PDO error mode to exception
       $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
       echo "Connected successfully";
